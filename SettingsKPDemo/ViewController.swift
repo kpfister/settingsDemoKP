7d4d7f8dcc476294089cd1242dcc03930cb8468d
kpfister <kpfister@me.com>
2018-05-08 14:08:00 -0600

    Initial Commit

---
+++ SettingsKPDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SettingsKPDemo
+//
+//  Created by Karl Pfister on 5/8/18.
+//  Copyright © 2018 Karl Pfister. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
